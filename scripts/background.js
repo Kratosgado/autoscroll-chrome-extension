0144b1aea1444431daf0d8cc7854d13256f793cf
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-09-05 08:50:01 +0000

    fix serviceWorker bug

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,22 @@
-
 chrome.action.onClicked.addListener((tab) => {
    chrome.scripting.executeScript({
-      target:  {tabId: tab.id },
-      function: openPopup
-   })
-})
-
-
-chrome.scripting.registerServiceWorker({
-   script: { src: 'backgroundServiceWorker.js' }
-});
+     target: {tabId: tab.id},
+     function: openPopup
+   });
+ });
+ 
+ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+   if (request.action === 'getOptions') {
+     // Simulate getting options (replace with your own logic)
+     const options = {
+       scrollTimeStart: 10,
+       scrollTimeEnd: 20,
+       tabsMin: 1,
+       tabsMax: 3,
+       subpagesMin: 1,
+       subpagesMax: 5,
+     };
+     sendResponse({ options });
+   }
+ });
+ 
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,23 +1,42 @@
-document.getElementById('startScroll').addEventListener('click', function () {
-   const scrollTimeStart = parseInt(document.getElementById('scrollTimeStart').value, 10);
-   const scrollTimeEnd = parseInt(document.getElementById('scrollTimeEnd').value, 10);
-   const tabsMin = parseInt(document.getElementById('tabsMin').value, 10);
-   const tabsMax = parseInt(document.getElementById('tabsMax').value, 10);
-   const subpagesMin = parseInt(document.getElementById('subpagesMin').value, 10);
-   const subpagesMax = parseInt(document.getElementById('subpagesMax').value, 10);
+// Select the button element by its ID
+const scrollButton = document.getElementById('startScroll');
 
-   const options = {
+// Check if the button exists
+if (scrollButton !== null) {
+  // Add a click event listener to the button
+  scrollButton.addEventListener('click', function () {
+    // Get values from input fields
+    const scrollTimeStart = parseInt(document.getElementById('scrollTimeStart').value, 10);
+    const scrollTimeEnd = parseInt(document.getElementById('scrollTimeEnd').value, 10);
+    const tabsMin = parseInt(document.getElementById('tabsMin').value, 10);
+    const tabsMax = parseInt(document.getElementById('tabsMax').value, 10);
+    const subpagesMin = parseInt(document.getElementById('subpagesMin').value, 10);
+    const subpagesMax = parseInt(document.getElementById('subpagesMax').value, 10);
+
+    // Define the options object
+    const options = {
       scrollTimeStart,
       scrollTimeEnd,
       tabsMin,
       tabsMax,
       subpagesMin,
       subpagesMax,
-   };
+    };
 
-   chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setOptions,
-      args: [ options ]
-   });
-});
+    // Execute the code with the options
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: function (options) {
+          // Your code here, you can access 'options' inside this function
+          console.log(options);
+        },
+        args: [options],
+      });
+    });
+  });
+} else {
+  // If the button does not exist, set the background color to red
+  document.body.style.backgroundColor = 'red';
+}
